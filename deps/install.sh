034d94c1d2ef71378c0674617e072134ce2c35da
Vitalijus Dobrovolskis <vitalijus.dobrovolskis@alumni.fh-aachen.de>
2022-06-02 01:38:20 +0200

    PCL als Abhängigkeit für das PLY-Auslesen hinzugefügt und den Installationsskript für Abhängigkeiten angepasst

---
+++ deps/install.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+set -euox pipefail
+
+CPU_CORES=$(nproc)
+
+pwd=$(pwd)
+INSTALL_DIR="$pwd/build"
+mkdir -p "${INSTALL_DIR}"
+
+BUILD_DIR="$pwd/cmake_build"
+mkdir -p "$BUILD_DIR"
+
+# eigen
+EIGEN_BUILD_DIR="$BUILD_DIR/eigen"
+mkdir -p "$EIGEN_BUILD_DIR"
+cd "$EIGEN_BUILD_DIR"
+cmake -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}" "$pwd/eigen"
+make install
+
+# cgal
+CGAL_BUILD_DIR="$BUILD_DIR/cgal"
+mkdir -p "$CGAL_BUILD_DIR"
+cd "$CGAL_BUILD_DIR"
+cmake -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}" "$pwd/cgal"
+make install
+
+# flann
+FLANN_SOURCE_DIR="$pwd/flann"
+cd "$FLANN_SOURCE_DIR"
+touch src/cpp/empty.cpp
+sed -e '/add_library(flann_cpp SHARED/ s/""/empty.cpp/' \
+    -e '/add_library(flann SHARED/ s/""/empty.cpp/' \
+    -e '/cuda_add_library(flann_cuda SHARED/ s/""/empty.cpp/' \
+    -i src/cpp/CMakeLists.txt
+
+FLANN_BUILD_DIR="$BUILD_DIR/flann"
+mkdir -p "$FLANN_BUILD_DIR"
+cd "$FLANN_BUILD_DIR"
+cmake -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}" \
+    -DBUILD_EXAMPLES=OFF \
+    -DBUILD_TESTS=OFF \
+    -DBUILD_DOC=OFF \
+    -DBUILD_C_BINDINGS=OFF \
+    -DBUILD_PYTHON_BINDINGS=OFF \
+    -DBUILD_MATLAB_BINDINGS=OFF \
+    -DBUILD_CUDA_LIB=OFF \
+    "$FLANN_SOURCE_DIR"
+
+make -j "$CPU_CORES"
+make install
+cd "$FLANN_SOURCE_DIR"
+git reset --hard
+rm -f src/cpp/empty.cpp
+
+# pcl
+PCL_BUILD_DIR="$BUILD_DIR/pcl"
+mkdir -p "$PCL_BUILD_DIR"
+cd "$PCL_BUILD_DIR"
+cmake -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}" \
+    "$pwd/cgal"
+make -j "$CPU_CORES"
+make install
+
+# ceres
+CERES_BUILD_DIR="$BUILD_DIR/ceres"
+mkdir -p "$CERES_BUILD_DIR"
+cd "$CERES_BUILD_DIR"
+cmake -DBUILD_TESTING=OFF \
+    -DBUILD_EXAMPLES=OFF \
+    -DBUILD_BENCHMARKS=OFF \
+    -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}" \
+    "$pwd/ceres"
+make -j "$CPU_CORES"
+make install
+
+# colmap
+COLMAP_SRC_DIR="$pwd/colmap"
+cd "$COLMAP_SRC_DIR"
+rm -f "cmake/FindEigen3.cmake"
+
+COLMAP_BUILD_DIR="$BUILD_DIR/colmap"
+mkdir -p "$COLMAP_BUILD_DIR"
+cd "$COLMAP_BUILD_DIR"
+cmake -DCUDA_NVCC_FLAGS="--std c++14" \
+    -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}" -DGUI_ENABLED=ON \
+    "$COLMAP_SRC_DIR"
+make -j "$CPU_CORES"
+make install
+
+cd "$COLMAP_SRC_DIR"
+git reset --hard
+
+cd "$pwd"
