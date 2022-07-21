3c58cda6f60504ab694e6b467ed7e8052aa2e7d6
Vitalijus Dobrovolskis <vitalijus.dobrovolskis@alumni.fh-aachen.de>
2022-07-21 22:43:45 +0200

    Namensanpassungen in der CI-Pipeline

--- lib/data/src/point_cloud_provider.cpp
+++ lib/data/src/point_cloud_provider.cpp
@@ -18,7 +18,6 @@ namespace rcsop::data {
 
     using Kernel = CGAL::Exact_predicates_inexact_constructions_kernel;
     using Point = Kernel::Point_3;
-    using SurfaceMesh = CGAL::Surface_mesh<Point>;
 
     PointCloudProvider::PointCloudProvider(const InputDataCollector& input,
                                            const camera_options& camera_options)
